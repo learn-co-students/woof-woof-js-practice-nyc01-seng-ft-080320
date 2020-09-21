708ea2a363f0374572da95570750ec9901fdf86f
anickel101 <anson.nickel@gmail.com>
2020-09-21 16:18:55 -0400

    4/5 Deliverables

---
+++ src/index.js
@@ -0,0 +1,83 @@
+const PUPS_BASE_URL = "http://localhost:3000/pups"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const getDogs = () => {
+        fetch(PUPS_BASE_URL)
+        .then(response => response.json())
+        .then(pups => renderPups(pups))
+    }
+
+    const renderPups = pups => {
+        pups.map(renderPup)
+    }
+
+    const renderPup = pup => {
+        const dogBar = document.querySelector("#dog-bar")
+        const dogSpan = document.createElement("span")
+        dogSpan.dataset.pup_id = pup.id
+        dogSpan.innerText = pup.name
+        dogBar.append(dogSpan)
+    }
+
+    const clickHandler = () => {
+        document.addEventListener("click", (e) => {
+            if (e.target.matches("span")) {
+                clearPupCard();
+                getDog(e.target.dataset.pup_id);
+            } else if (e.target.matches(".dog-button")) {
+                toggleGoodBad(e.target.dataset.id);
+            }
+        })
+    }
+
+    const getDog = (id) => {
+        fetch(PUPS_BASE_URL+"/"+id)
+        .then(response => response.json())
+        .then(pup => showPupCard(pup))
+    }
+
+    const showPupCard = (pup) => {
+        const pupCardDiv = document.querySelector("#dog-info")
+        let attitude = (pup.isGoodDog === true) ? "Bad" : "Good"
+        pupCardDiv.innerHTML =
+        `<img src=${pup.image}>
+        <h2>${pup.name}</h2>
+        <button class="dog-button" data-id=${pup.id}>${attitude} Dog!</button>`
+    }
+
+    const clearPupCard = () => {
+        const pupCardDiv = document.querySelector("#dog-info")
+        pupCardDiv.innerHTML = ""
+    }
+
+
+    const toggleGoodBad = (pup_id) => {
+
+        const url = `${PUPS_BASE_URL}/${pup_id}`
+
+        const dogDiv = document.querySelector("#dog-info")
+        const attitude = dogDiv.querySelector("button").textContent.split(" ")[0]
+        let opposite = (attitude === "Good") ? true : false
+
+        const pupObj = {
+            isGoodDog: opposite
+        }
+
+        const options = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(pupObj)
+        }
+
+        fetch(url, options)
+        .then(response => response.json())
+        .then(pup => showPupCard(pup))
+    }
+
+    clickHandler();
+    getDogs();
+})
